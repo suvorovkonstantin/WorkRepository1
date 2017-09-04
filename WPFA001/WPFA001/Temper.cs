4c4714308a8c5433e5bd36dfa18cd30277720d18
Konstantine Suvorov <suvorov4815162342@yandex.ru>
2017-09-04 12:23:29 +0300

    second commit

---
+++ WPFA001/WPFA001/Temper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WPFA001
+{
+    class Temper : MainWindow
+    {
+        async public Task<int> StartTask()
+        {
+            Thread.Sleep(5000);
+             MessageBox.Show("Async mess");
+            return 1;
+        }
+    }
+}
